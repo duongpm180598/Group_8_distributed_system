d2518c13848592c8f65d783189d476a0bb1dfb78
Dng <duongpm180598@gmail.com>
2025-08-06 01:09:28 +0700

    fix bug

--- Assignment/src/Frontend/src/views/HomeView.vue
+++ Assignment/src/Frontend/src/views/HomeView.vue
@@ -66,7 +66,7 @@
           Thiết kế gần đây
         </p>
 
-        <div class="grid grid-cols-3 md:grid-cols-6 gap-4">
+        <div class="grid grid-cols-3 md:grid-cols-6 gap-4" v-if="designs.length > 0">
           <template v-for="design in designs" :key="design.designId">
             <div class="relative group min-h-[250px] h-full">
               <a :href="'/design/' + design.designId" class="block h-full">
@@ -89,9 +89,7 @@
               </a>
 
               <button
-                data-modal-target="popup-modal"
-                data-modal-toggle="popup-modal"
-                @click.prevent="setSelectedDesign(design.designId)"
+                @click="setSelectedDesign(design.designId)"
                 class="cursor-pointer absolute top-2 right-2 bg-red-600 hover:bg-red-700 text-white p-1 rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-300"
               >
                 <svg
@@ -113,74 +111,73 @@
       </div>
     </div>
   </main>
-  <template v-if="designs.length > 0">
+  <template v-if="openModal">
     <div
-    id="popup-modal"
-    tabindex="-1"
-    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-  >
-    <div class="relative p-4 w-full max-w-md max-h-full">
-      <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
-        <button
-          type="button"
-          class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-          data-modal-hide="popup-modal"
-        >
-          <svg
-            class="w-3 h-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-            />
-          </svg>
-          <span class="sr-only">Close modal</span>
-        </button>
-        <div class="p-4 md:p-5 text-center">
-          <svg
-            class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-          <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-            Bạn có chắc muốn xóa thiết kế này không?
-          </h3>
+      tabindex="-1"
+      class="fixed top-0 right-0 left-0 bottom-0 z-50 flex justify-center items-center backdrop-blur-sm bg-opacity-50"
+    >
+      <div class="relative p-4 w-full max-w-md max-h-full">
+        <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
           <button
-            data-modal-hide="popup-modal"
             type="button"
-            class="py-2.5 px-5 me-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-          >
-            Hủy
-          </button>
-          <button
-            @click="handleDelete()"
+            class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
             data-modal-hide="popup-modal"
-            type="button"
-            class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
           >
-            Xóa
+            <svg
+              class="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+            <span class="sr-only">Close modal</span>
           </button>
+          <div class="p-4 md:p-5 text-center">
+            <svg
+              class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Bạn có chắc muốn xóa thiết kế này không?
+            </h3>
+            <button
+              @click="closeModal()"
+              type="button"
+              class="py-2.5 px-5 me-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            >
+              Hủy
+            </button>
+            <button
+              @click="handleDelete()"
+              data-modal-hide="popup-modal"
+              type="button"
+              class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+            >
+              Xóa
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   </template>
 </template>
 
@@ -188,15 +185,19 @@
 import { onMounted, ref } from 'vue'
 import router from '@/router'
 import { createOrUpdateDesign, deleteDesignById, getDesigns } from '@/services/design.service'
+import { initModals } from 'flowbite'
 
 const designs = ref([])
 const originDesigns = ref([])
 const selectedId = ref('')
+const openModal = ref(false)
 
 onMounted(() => {
+  initModals()
   getAllDesigns()
 })
 const setSelectedDesign = (designId) => {
+  openModal.value = true
   selectedId.value = designId
 }
 const getAllDesigns = async () => {
@@ -209,7 +210,9 @@ const createDesign = async () => {
   const res = await createOrUpdateDesign({})
   router.push(`/design/${res.designId}`)
 }
-
+const closeModal = () => {
+  openModal.value = false
+}
 const handleDelete = async () => {
   const id = selectedId.value
   if (id) {
@@ -217,6 +220,7 @@ const handleDelete = async () => {
     if (res) {
       selectedId.value = ''
       await getAllDesigns()
+      closeModal()
     }
   }
 }
